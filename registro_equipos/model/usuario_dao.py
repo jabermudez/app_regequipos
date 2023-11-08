6fb0ff22290db3edee15044d4880014295d9bd00
Alex <jabermudez99@misena.edu.co>
2023-11-07 23:03:48 -0500

    Creación tabla Equipo

--- registro_equipos/model/usuario_dao.py
+++ registro_equipos/model/usuario_dao.py
@@ -5,30 +5,48 @@ from tkinter import messagebox
 
 def crear_tabla():
     conexion = ConexionDB()
+    
+    sql_equipos = '''
+    CREATE TABLE IF NOT EXISTS equipos(
+        id_equipo INTEGER PRIMARY KEY AUTOINCREMENT,
+        codigo_equipo INTEGER,
+        marca_equipo VARCHAR(100),
+        serial_equipo VARCHAR(100),
+        placa_equipo INTERGER
+        
+    )'''
 
-    sql = '''
-    CREATE TABLE usuarios(
-        id_usuario INTEGER,
+    sql_usuarios = '''
+    CREATE TABLE IF NOT EXISTS usuarios(
+        id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
         codigo INTEGER,
         nombre VARCHAR(100),
         apellidos VARCHAR(100),
         documento INTEGER,
         ficha VARCHAR(50),
         correo VARCHAR(100),
-        celular INTEGER,
-        codigo_equipo FOREIGN KEY (codigo_equipo) REFERENCES equipos(codigo_equipo),
-        PRIMARY KEY(id_usuario AUTOINCREMENT)
+        celular INTEGER,        
+        id_equipo INTEGER,
+        FOREIGN KEY (id_equipo) REFERENCES equipos(id_equipo)
+    
     )'''
 
     try:
-        conexion.cursor.execute(sql)
+        # Crear la tabla equipos
+        conexion.cursor.execute(sql_equipos)
+
+        # Crear la tabla usuarios
+        conexion.cursor.execute(sql_usuarios)
+
+        # Confirmar cambios
         conexion.cerrar()
+
         titulo = 'Crear Registro'
-        mensaje = 'Se creo la tabla en la base de daos'
+        mensaje = 'Se crearon las tablas en la base de datos'
         messagebox.showinfo(titulo, mensaje)
-    except:
+    except Exception as e:
         titulo = 'Crear Registro'
-        mensaje = 'La tabla ya existe'
+        mensaje = 'Error al crear las tablas: ' + str(e)
         messagebox.showwarning(titulo, mensaje)
 
 #Permite borrar la tabla usuarios desde el aplicativo
@@ -131,6 +149,88 @@ def buscar(codigo):
     conexion.cerrar()
     
     return usuario
+
+#Se crea la clase equipo
+
+class Equipo:
+    def __init__(self, codigo_equipo, marca, serial_equipo, placa_equipo):
+        self.id_equipo = None
+        self.codigo_equipo = codigo_equipo
+        self.marca = marca
+        self.serial_equipo = serial_equipo
+        self.placa_equipo = placa_equipo
+        
+    def __str__(self):
+        return f'Usuario[{self.codigo_equipo},{self.marca}, {self.serial_equipo}, {self.placa_equipo}]'
+
+def guardar_equipo(equipo):
+    conexion = ConexionDB()
+
+    sql =f"""INSERT INTO equipos (codigo_equipo, marca, serial_equipo, placa_equipo)
+    VALUES({equipo.codigo},'{equipo.marca}','{equipo.serial_equipo}', {equipo.placa_equipo})"""
+
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+    except:
+        titulo = 'Conexiòn al Registro'
+        mensaje = 'La tabla equipos no se encuentra registrada'
+        messagebox.showerror(titulo, mensaje)
+
+def listar_equipo():
+
+    conexion = ConexionDB()
+
+    lista_equipos = []
+    sql = 'SELECT * FROM equipos'
+
+    try:
+        conexion.cursor.execute(sql)
+        lista_equipos = conexion.cursor.fetchall()
+        conexion.cerrar()
+    except:
+        titulo = 'Conexiòn al Registro'
+        mensaje = 'Crea la tabla en la base de datos'
+        messagebox.showwarning(titulo, mensaje)
+    
+    return lista_equipos
+
+def editar_equipo(equipo, id_equipo):
+    conexion = ConexionDB()
+
+    sql = f"""UPDATE usuarios 
+    SET codigo_equipo = {equipo.codigo}, marca ='{equipo.marca_equipo}', serial_equipo ='{equipo.serial_equipo}', placa_equipo = {equipo.placa} WHERE id_equipo = {id_equipo}"""
+
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+    except:
+        titulo = 'Edición de datos'
+        mensaje = 'No se ha podido editar este registro'
+        messagebox.showerror(titulo, mensaje)
+
+def eliminar_equipo(id_equipo):
+    conexion = ConexionDB()
+    sql = f'DELETE FROM equipos WHERE id_equipo = {id_equipo}'
+
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+    except:
+        titulo = 'Eliminar datos'
+        mensaje = 'No se pudo eliminar este registro'
+        messagebox.showerror(titulo, mensaje)
+
+def buscar_equipo(codigo_equipo):
+    conexion = ConexionDB()    
+        
+    # Consultar la base de datos
+    conexion.cursor.execute("SELECT * FROM equipos WHERE codigo_equipo = ?", (codigo_equipo,))
+    
+    equipo = conexion.cursor.fetchone()    
+    conexion.cerrar()
+    
+    return equipo
     
     
    
--- registro_equipos/registro.py
+++ registro_equipos/registro.py
@@ -1,18 +1,18 @@
 import tkinter as tk
 import ttkbootstrap as tb
-from usuarios.interfaz import Frame, barra_menu
+from usuarios.interfaz2 import Frame1, barra_menu
 
 def main():
     root = tb.Window(themename="morph")
     #root = tk.Tk()
-    root.geometry("1100x700+400+150")  
+    root.geometry("1100x700+100+100")  
     root.title('Registro de Equipos de Computo')
     root.iconbitmap('img/nuevo_logo.ico')
-    #root.resizable(0,0)
+    root.resizable(0,0)
     
     barra_menu(root)
 
-    app = Frame(root = root)
+    app = Frame1(root = root)
 
     app.mainloop()
 
